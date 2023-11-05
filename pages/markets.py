b973c68dcaba2ef080969fa03ce5adad7c8cb5b0
Miguel <miguelmayhem92@gmail.com>
2023-11-05 20:23:15 +0100

    new plots

--- pages/markets.py
+++ pages/markets.py
@@ -33,20 +33,50 @@ index = st.selectbox(
 tabs = st.tabs(['overview'])
 
 if debug_mode:
+    local_storage = configs["local_storage"]
     if st.button('Launch'):
         for tab in tabs:
             with tab:
-                fig = plotly.io.read_json(f'C:/Users/Miguel/Dropbox/virgo/{index}/panel_signals.json')
-                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+                try:
+                    with open(f'{local_storage}/virgo/{index}/market_message.json') as json_file:
+                        market_message = json.load(json_file)
+                    message1 = market_message['current_state']
+                    message2 = market_message['current_step_state']
+                    message3 = market_message['report_date']
+                    st.write(f"status:")
+                    st.write(f"current state: {message1}")
+                    st.write(f"current step in the state: {message2}")
+                    st.write(f"report date: {message3}")
+                except:
+                    st.write("no text was recorded :(")
+                try:
+                    fig = plotly.io.read_json(f'{local_storage}/virgo/{index}/panel_signals.json')
+                    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+                except:
+                    st.write("no plot available :(")
 else:
     if st.button('Launch'):
         for tab in tabs:
             with tab:
                 conn = st.connection('s3', type=FilesConnection)
-                jsonfile = conn.read(f"virgo-data/market_plots/{index}/panel_signals.json", input_format="json")
-                json_dump = json.dumps(jsonfile)
-                fig = plotly.io.from_json(json_dump)
-                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+                try:
+                    market_message = conn.read(f"virgo-data/market_plots/{index}/market_message.json", input_format="json")
+                    message1 = market_message['current_state']
+                    message2 = market_message['current_step_state']
+                    message3 = market_message['report_date']
+                    st.write(f"status:")
+                    st.write(f"current state: {message1}")
+                    st.write(f"current step in the state: {message2}")
+                    st.write(f"report date: {message3}")
+                except:
+                    st.write("no text was recorded :(")
+                try:
+                    jsonfile = conn.read(f"virgo-data/market_plots/{index}/panel_signals.json", input_format="json")
+                    json_dump = json.dumps(jsonfile)
+                    fig = plotly.io.from_json(json_dump)
+                    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+                except:
+                    st.write("no plot available :(")
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
