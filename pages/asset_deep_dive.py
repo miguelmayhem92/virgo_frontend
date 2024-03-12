316895e3fb9e032428b071f3598f4d517bcacd0a
Miguel <miguelmayhem92@gmail.com>
2024-03-12 14:51:19 +0100

    date short

--- pages/asset_deep_dive.py
+++ pages/asset_deep_dive.py
@@ -17,7 +17,7 @@ configs = yaml.safe_load(Path('configs.yaml').read_text())
 debug_mode = configs["debug_mode"]
 asset_plots = configs["asset_plots"]
 execution_date = datetime.datetime.today().strftime('%Y-%m-%d')
-execution_date = f"report date: {execution_date}"
+execution_date = f"{execution_date}"
 bucket = 'virgo-data'
 
 st.set_page_config(layout="wide")
