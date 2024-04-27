7e4abeac29690fd5a623269d7ecebc47ba61921b
Miguel <miguelmayhem92@gmail.com>
2024-04-27 15:10:26 +0200

    improved market plots

--- pages/markets.py
+++ pages/markets.py
@@ -4,8 +4,9 @@ import json
 import yaml
 from pathlib import Path
 from utils import get_connection
-from utils import logo, print_object
+from utils import logo, print_object, dowload_any_object, get_connection
 
+from virgo_modules.src.re_utils import produce_plotly_plots
 import time
 
 configs = yaml.safe_load(Path('configs.yaml').read_text())
@@ -16,6 +17,7 @@ market_indexes = {k:v for list_item in market_indexes for (k,v) in list_item.ite
 st.set_page_config(layout="wide")
 logo(debug_mode)
 
+bucket = 'virgo-data'
 
 st.markdown("# Markets analysis")
 
@@ -45,7 +47,7 @@ market_plots_ = list(market_plots.keys())
 options = st.multiselect(
     'select you plot options',
     market_plots_,
-    market_plots_[0:3]
+    market_plots_[0:4]
 )
 
 tab_overview, = st.tabs(['overview'])
@@ -58,18 +60,62 @@ if st.button('Launch'):
     with st.spinner('.......................... Now loading ..........................'):
         time.sleep(2)
         if debug_mode:
-            local_storage = configs["local_tmps_market_research"]
+            local_storage = False
             conn = False
+            streamlit_conn = False
         else:
             conn = get_connection()
             local_storage = False
-            
-        with tab_overview:
-            for plot_name in options:
-                object = market_plots[plot_name]
-                name = object['name']
-                type_ = object['data_type']
+            streamlit_conn = True
+        
+        data_frame = dowload_any_object('dataframe.csv', f'market_plots/{index_symbol}/', 'csv',bucket)
+        t_matrix = dowload_any_object('tmatrix.txt', f'market_plots/{index_symbol}/', 'txt', bucket)
+        data_configs = dowload_any_object('asset_configs.json', f'market_plots/{index_symbol}/', 'json',bucket)
+        forecastings = dowload_any_object('batch_predictions_csv.csv', f'market_plots/{index_symbol}/', 'csv',bucket)
 
-                print_object(name, type_, index_symbol, debug_mode, local_storage, conn)
+        plotter = produce_plotly_plots(index_symbol, data_frame,data_configs,save_path = False,save_aws = False,show_plot = False, return_figs = True)
+
+        with tab_overview:
+
+            for plot_name in options:
+
+                # object = market_plots[plot_name]
+                # name = object['name']
+                # type_ = object['data_type']
+                # method = object['method']
+
+                # print_object(name, type_, index_symbol, debug_mode, local_storage, conn)
+
+                if plot_name == "panel signals":
+                    features_ = ['volatility_log_return', 'rel_MA_spread', 'target_mean_dow','pair_z_score','RSI','ROC','STOCHOSC']
+                    spread_column = 'relative_spread_ma'
+                    fig = plotter.plot_asset_signals(features_, spread_column ,date_intervals = False)
+                    st.plotly_chart(fig, use_container_width=True)
+
+                elif plot_name == "current state":
+                    _,jsnom = plotter.plot_hmm_analysis(settings = data_configs, t_matrix = t_matrix)
+                    st.write(jsnom)
+
+                elif plot_name == 'states analysis':
+                    fig,_ = plotter.plot_hmm_analysis(settings = data_configs, t_matrix = t_matrix)
+                    st.plotly_chart(fig, use_container_width=True)
+                    
+                elif plot_name == "states scaled time series":
+                    fig = plotter.explore_states_ts()
+                    st.plotly_chart(fig, use_container_width=True)
+                
+                elif plot_name == "forecastings":
+                    fig = plotter.produce_forecasting_plot(forecastings)
+                    st.plotly_chart(fig, use_container_width=True)
+
+                # features_ = ['volatility_log_return', 'rel_MA_spread','RSI']
+                # spread_column = 'relative_spread_ma'
+        
+                # plotter = produce_plotly_plots(ticket_name, data_frame,settings, save_path, save_aws = save_aws, show_plot= False, aws_credentials = credentials)
+                # plotter.produce_forecasting_plot(batch_predictions_csv)
+                # plotter.plot_asset_signals(features_, spread_column)
+                # plotter.explore_states_ts()
+                # plotter.plot_hmm_analysis(settings = settings, t_matrix = t_matrix, model = model)
+
         
 st.button("Re-run")
